165f74429f434f4bf228f809cb51990e82a4943c
Daniel Gilchrist <danniieelg@gmail.com>
2024-09-09 18:56:45 +0100

    add some more tools to auto install + vim-haml

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -3,6 +3,13 @@ return {
   opts = {
     ensure_installed = {
       "crystalline",
+      "gopls",
+      "rubocop",
+      "ruby-lsp",
+      "rust-analyzer",
+      "sorbet",
+      "terraform-ls",
+      "yaml-language-server",
     },
   },
 }
