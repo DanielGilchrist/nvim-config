a2c9a5f3b09d4a2dd9aabc328324d9e90cac4fd2
Daniel Gilchrist <daniel@workforce.com>
2024-09-09 13:20:09 +0100

    Initial config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", telescope.find_files, {})
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
+vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "ruby", "rust", "go", "lua", "vim", "vimdoc", "javascript", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        auto_install = false,
+        ignore_install = {},
+      })
+    end,
+  },
+}
