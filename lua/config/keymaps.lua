fe308e8c9b1b8a0df5f17e7afa6d47d55e033cc4
Daniel Gilchrist <danniieelg@gmail.com>
2024-09-23 00:39:35 +0100

    Add keymap to search current file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,10 +5,13 @@
 local map = vim.keymap.set
 local telescope = require("telescope.builtin")
 
-map("n", "<C-p>", telescope.find_files, {})
-map("n", "<leader>fg", telescope.live_grep, {})
-map("n", "<leader>fb", telescope.buffers, {})
-map("n", "<leader>fh", telescope.help_tags, {})
+map("n", "<C-p>", telescope.find_files, { desc = "File Picker" })
+map("n", "<leader>fg", telescope.live_grep, { desc = "Search current working directory" })
+map("n", "<leader>fC", function()
+  telescope.live_grep({ search_dirs = { vim.fn.expand("%:p") } })
+end, { desc = "Search current file" })
+map("n", "<leader>fb", telescope.buffers, { desc = "Search buffers" })
+map("n", "<leader>fh", telescope.help_tags, { desc = "Search help" })
 
 map("n", "<leader>uC", function()
   vim.cmd("doautocmd User LazyColorscheme")
