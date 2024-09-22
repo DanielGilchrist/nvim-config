d4488dbc40459c50c22e37ed820b7563ce9c8cca
Daniel Gilchrist <danniieelg@gmail.com>
2024-09-22 11:04:07 +0100

    Change to ayu theme

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,15 +1,25 @@
 vim.g.material_style = "deep ocean"
 
 return {
-  -- { "scottmckendry/cyberdream.nvim", name = "cyberdream" },
+  {
+    "scottmckendry/cyberdream.nvim",
+    name = "cyberdream",
+  },
   {
     "marko-cerovac/material.nvim",
     name = "material",
   },
   {
+    "Shatur/neovim-ayu",
+    name = "ayu",
+    opts = {
+      mirage = true,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "material",
+      colorscheme = "ayu",
     },
   },
 }
