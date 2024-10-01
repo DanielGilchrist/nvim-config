4de57cdcbb66c31812fe3843b0e25684e2908151
Daniel Gilchrist <daniel@workforce.com>
2024-10-01 14:50:26 +0100

    Disable auto format for ruby_lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -12,6 +12,12 @@ return {
           disable_format(client)
         end
       end,
+      -- TODO: See if autoformat can be kept while respecting project config
+      ruby_lsp = function(_, opts)
+        opts.on_attach = function(client)
+          disable_format(client)
+        end
+      end,
     },
   },
 }
