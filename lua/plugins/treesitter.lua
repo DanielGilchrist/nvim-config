e87c26349f0771ac56817809c12f1a06b2d045ff
Daniel Gilchrist <daniel@workforce.com>
2024-10-15 09:39:03 +0100

    Disable treesitter indent
    Driving me up the wall

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
         ensure_installed = { "ruby", "rust", "go", "lua", "vim", "vimdoc", "javascript", "html" },
         sync_install = false,
         highlight = { enable = true },
-        indent = { enable = true },
+        indent = { enable = false },
         auto_install = false,
         ignore_install = {},
       })
