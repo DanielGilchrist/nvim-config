47113d018703c554fd8b3b08656b704ea43fcdbd
Daniel Gilchrist <daniel@workforce.com>
2024-10-17 13:30:54 +0100

    Configure lsp's that may depend on being in Gemfile to not run if they aren't

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,6 +5,38 @@ local function disable_format(client)
   client.server_capabilities.documentRangeFormattingProvider = false
 end
 
+local function cmd_noop()
+  return "echo"
+end
+
+local function normalise_command(cmd)
+  if type(cmd) == "string" then
+    cmd = cmd == "" and cmd_noop() or cmd
+    return { cmd }
+  elseif type(cmd) == "table" then
+    return cmd
+  else
+    error("cmd must be a string or a table!: " .. vim.inspect(cmd))
+  end
+end
+
+local function gemfile_command_or_fallback(gem_name, command, fallback)
+  command = command == nil and normalise_command(gem_name) or normalise_command(command)
+  fallback = fallback == nil and command or normalise_command(fallback)
+
+  local gemfile_lock = vim.fn.findfile('Gemfile.lock', '.;')
+
+  if gemfile_lock ~= "" then
+    local is_installed = vim.fn.systemlist("grep '" .. gem_name .. "' " .. gemfile_lock)
+
+    if not vim.tbl_isempty(is_installed) then
+      return vim.list_extend({ "bundle", "exec" }, command)
+    end
+  end
+
+  return fallback
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -21,12 +53,14 @@ return {
     },
     servers = {
       rubocop = {
-        cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        cmd = gemfile_command_or_fallback("rubocop", { "rubocop", "--lsp" }, ""),
         root_dir = lspconfig.util.root_pattern("Gemfile", ".git", ".")
       },
-      ruby_lsp = {},
+      ruby_lsp = {
+        cmd = gemfile_command_or_fallback("ruby-lsp")
+      },
       sorbet = {
-        cmd = { "bundle", "exec", "srb", "tc", "--lsp" }
+        cmd = gemfile_command_or_fallback("sorbet", { "srb", "tc", "--lsp" }, "")
       },
       yamlls = {
         settings = {
