27c59bb0c420b982e413eb4317b146a807859aaa
Daniel Gilchrist <daniel@workforce.com>
2024-10-27 20:13:21 +0000

    Enable relative numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- vim.lsp.set_log_level("debug")
+
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
 
@@ -11,4 +13,4 @@ vim.filetype.add({
   },
 })
 
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
