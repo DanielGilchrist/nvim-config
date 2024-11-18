e3a1e8595ccd671e3a29dff859f801f81a5fc685
Daniel Gilchrist <daniel@workforce.com>
2024-11-18 11:48:50 +0000

    Add Ytestn user command

--- lua/custom/yank_test_line.lua
+++ lua/custom/yank_test_line.lua
@@ -13,17 +13,63 @@ local function maybe_test_name()
   return file_path:match("test/(.*)_test%.rb")
 end
 
+local function build_test_command(test_name, line_number)
+  local test_command = "test " .. double_quote(test_name)
+
+  if line_number then
+    test_command = test_command .. ", " .. line_number
+  end
+
+  return test_command
+end
+
+local function save_to_clipboard_and_notify(test_command)
+  vim.fn.setreg("+", test_command)
+  noice.notify(test_command, "success")
+end
+
+local function not_a_test_file()
+  noice.notify("Not a test file!", "warn")
+end
+
+local function find_test_line_number()
+  local cursor_line = vim.fn.line(".")
+
+  while cursor_line > 0 do
+    local line_content = vim.fn.getline(cursor_line)
+    if line_content:match("^%s*test%s") then
+      return cursor_line
+    end
+
+    cursor_line = cursor_line - 1
+  end
+end
+
 local function yank_test_line()
   local test_name = maybe_test_name()
 
   if test_name then
-    local test_command = "test " .. double_quote(test_name)
-
-    vim.fn.setreg("+", test_command)
-    noice.notify(test_command, "success")
+    local test_command = build_test_command(test_name)
+    save_to_clipboard_and_notify(test_command)
   else
-    noice.notify("Not a test file", "warn")
+    not_a_test_file()
+  end
+end
+
+local function yank_test_line_with_number()
+  local test_name = maybe_test_name()
+
+  if test_name then
+    local line_number = find_test_line_number()
+
+    if line_number then
+      local test_command = build_test_command(test_name, line_number)
+      save_to_clipboard_and_notify(test_command)
+    end
+  else
+    not_a_test_file()
   end
 end
 
 vim.api.nvim_create_user_command("Ytest", yank_test_line, {})
+vim.api.nvim_create_user_command("Ytestn", yank_test_line_with_number, {})
