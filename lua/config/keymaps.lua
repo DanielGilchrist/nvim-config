4b9d2a76770dbd3b0ceee21136478a3350b1ed3e
Daniel Gilchrist <daniel@workforce.com>
2024-11-27 09:36:23 +0000

    Lazy load gitlinker and telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,33 +4,47 @@
 
 local map = vim.keymap.set
 
-local gitlinker = require("gitlinker")
-local telescope = require("telescope.builtin")
-local telescope_ext = require("telescope").extensions
+-- Telescope keymaps
+map("n", "<C-p>", function()
+  require("telescope.builtin").find_files()
+end, { desc = "File Picker" })
 
-map("n", "<C-p>", telescope.find_files, { desc = "File Picker" })
-map("n", "<leader>fg", telescope_ext.live_grep_args.live_grep_args, { desc = "Search current working directory" })
+map("n", "<leader>fg", function()
+  require("telescope").extensions.live_grep_args.live_grep_args()
+end, { desc = "Search current working directory" })
+
 map("n", "<leader>fC", function()
-  telescope.live_grep({ search_dirs = { vim.fn.expand("%:p") } })
+  require("telescope.builtin").live_grep({ search_dirs = { vim.fn.expand("%:p") } })
 end, { desc = "Search current file" })
-map("n", "<leader>fb", telescope.buffers, { desc = "Search buffers" })
-map("n", "<leader>fh", telescope.help_tags, { desc = "Search help" })
+
+map("n", "<leader>fb", function()
+  require("telescope.builtin").buffers()
+end, { desc = "Search buffers" })
+
+map("n", "<leader>fh", function()
+  require("telescope.builtin").help_tags()
+end, { desc = "Search help" })
 
 map("n", "<leader>uC", function()
   vim.cmd("doautocmd User LazyColorscheme")
-  telescope.colorscheme({ enable_preview = true })
+  require("telescope.builtin").colorscheme({ enable_preview = true })
 end, { desc = "View installed colour themes" })
 
+-- Gitlinker keymaps
 map("n", "<leader>gy", function()
-  local url = gitlinker.get_buf_range_url("n")
+  local url = require("gitlinker").get_buf_range_url("n")
   vim.fn.setreg("+", url)
 end, { desc = "Copy remote link to clipboard", noremap = true })
 
 map("v", "<leader>gy", function()
-  local url = gitlinker.get_buf_range_url("v")
+  local url = require("gitlinker").get_buf_range_url("v")
   vim.fn.setreg("+", url)
 end, { desc = "Copy remote link to clipboard", noremap = true })
 
-map("n", "<leader>fd", telescope.diagnostics, { desc = "Find diagnostics" })
+-- Diagnostic keymaps
+map("n", "<leader>fd", function()
+  require("telescope.builtin").diagnostics()
+end, { desc = "Find diagnostics" })
+
 map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to prev diagnostic" })
 map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -1,6 +1,7 @@
 return {
   "ruifm/gitlinker.nvim",
-  requires = "nvim-lua/plenary.nvim",
+  keys = { "<leader>gy", mode = { "n", "v" } },
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("gitlinker").setup({
       mappings = nil,
