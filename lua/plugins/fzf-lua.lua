0f6c5e73a64fe2f0ef991228e2821903c233f33a
Daniel Gilchrist <daniel@workforce.com>
2024-12-20 13:15:12 +0000

    Hide shit colourschemes

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,6 +1,35 @@
+local shit_colourschemes = {
+  "^blue$",
+  "^darkblue$",
+  "^default$",
+  "^delek$",
+  "^desert$",
+  "^elflord$",
+  "^evening$",
+  "^habamax$",
+  "^industry$",
+  "^koehler$",
+  "^lunaperche$",
+  "^morning$",
+  "^murphy$",
+  "^pablo$",
+  "^peachpuff$",
+  "^quiet$",
+  "^ron$",
+  "^shine$",
+  "^slate$",
+  "^sorbet$",
+  "^torte$",
+  "^vim$",
+  "^wildcharms$",
+  "^zaibatsu$",
+  "^zellners$",
+}
+
 return {
   "ibhagwan/fzf-lua",
   opts = {
+    colorschemes = { ignore_patterns = shit_colourschemes },
     grep = {
       rg_glob = true, -- Allows filtering by filetype in grep with `foo -- *.rb` for example
     }
