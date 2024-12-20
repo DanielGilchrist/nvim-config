454998cb000ecc1a0201e8654b6d1ca2c516fc3e
Daniel Gilchrist <daniel@workforce.com>
2024-12-20 23:59:22 +0000

    Update blink.cmp + make completion stay out of the way unless I press tab

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,9 +1,18 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<Up>"] = { "fallback" },
+      ["<Down>"] = { "fallback" },
+      ["<Left>"] = { "fallback" },
+      ["<Right>"] = { "fallback" },
+    },
     completion = {
-      list = {
-        selection = "manual"
+      trigger = {
+        show_on_insert_on_trigger_character = false,
       }
     }
   }
